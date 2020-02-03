30f7d0bae72d69a9a3813700868357a7ce430c74
akzhy <akshay48884@gmail.com>
2020-02-03 10:24:59 +0530

    updates

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,29 +1,33 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { FiUser as User } from "react-icons/fi"
+import { AiOutlineTrophy as Trophy } from "react-icons/ai"
+import { FaMoneyBillWaveAlt as Money } from "react-icons/fa"
+
+
 import Layout from "../components/layout"
 
 import { generalGuidelines } from "../../data/base.json"
 
 import "../styles/post.scss"
 
-export default ({data}) => {
-
+export default ({ data }) => {
     const seo = {
         title: data.markdownRemark.frontmatter.title,
         description: data.markdownRemark.frontmatter.description,
-        image: data.markdownRemark.frontmatter.image.publicURL
+        image: data.markdownRemark.frontmatter.image.publicURL,
     }
 
     const rules = data.markdownRemark.frontmatter.rules.map((item, i) => {
-        return (
-            <Rule number={i+1} rule={item} key={"rule"+i}/>
-        )
+        return <Rule number={i + 1} rule={item} key={"rule" + i} />
     })
 
-    const allGeneralGuidelines = generalGuidelines.map((item,i) => {
-        return (
-            <Rule number={i+1} rule={item} key={"generalg"+i}/>
-        )
+    const allGeneralGuidelines = generalGuidelines.map((item, i) => {
+        return <Rule number={i + 1} rule={item} key={"generalg" + i} />
+    })
+
+    const eventHeads = data.markdownRemark.frontmatter.eventHeads.map((item,i) => {
+        return <DetailCard icon={<User />} rep="event head" content={item.name} phone={item.phone} key={`detail-card-eh-${i}`}/>
     })
 
     return (
@@ -32,17 +36,30 @@ export default ({data}) => {
                 <div className="title">
                     <h1>{data.markdownRemark.frontmatter.title}</h1>
                 </div>
-                <div className="page-section row page">
-                    <div dangerouslySetInnerHTML={{
-                        __html: data.markdownRemark.html
-                    }}></div>
-                    <div className="row rules">
-                        <div className="title">
-                            <h3>Rules</h3>
+                <div className="page-section page">
+                    <div className="row">
+                        <div className="col s12 m6 l7">
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: data.markdownRemark.html,
+                                }}
+                            ></div>
+                            <div className="details">
+                                {eventHeads}
+                                <DetailCard className="fill" icon={<Trophy />} rep="prize" content={data.markdownRemark.frontmatter.prize}/>
+                                <DetailCard className="fill" icon={<Money />} rep="registration fee" content={data.markdownRemark.frontmatter.registration}/>
+                            </div>
                         </div>
-                        {rules}
+                        <div className="col s12 m6 l5">
+                            <div className="rules">
+                                <div className="title">
+                                    <h3>Rules</h3>
+                                </div>
+                                {rules}
+                            </div>
+                        </div>
                     </div>
-                    <div className="row general-guidelines rules">
+                    <div className="general-guidelines rules">
                         <div className="title">
                             <h3>General Guidelines</h3>
                         </div>
@@ -72,18 +89,42 @@ export const query = graphql`
                     }
                 }
                 rules
+                eventHeads {
+                    name
+                    phone
+                }
+                prize
+                registration
             }
         }
     }
 `
 
-
-const Rule = (data) => {
-    return(
+const Rule = data => {
+    return (
         <div className="rule card">
             <div className="box">
                 <span className="number">{data.number}</span>
                 <div className="content">{data.rule}</div>
+            </div>
+        </div>
+    )
+}
+
+const DetailCard = data => {
+    return (
+        <div className={`card ${data.className || ''}`}>
+            <div className="box">
+                <span className="icon">
+                    {data.icon}
+                </span>
+                <div className="content">
+                    <span className="rep">{data.rep}</span>
+                    <p>{data.content}</p>
+                    {data.phone &&
+                        <a href={`tel:${data.phone}`}>{data.phone}</a>
+                    }
+                </div>
             </div>
         </div>
     )
